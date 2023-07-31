272e73789023abc1f8ab043a657a7a98dc03000b
Sohel Rana <sohelrana1304@gmail.com>
2023-07-31 18:56:27 +0530

    Buying Guide Article Prompt has been updated

--- AmazonProductReview/AmazonProductBuyingGuide/ProductBuyingGuide.js
+++ AmazonProductReview/AmazonProductBuyingGuide/ProductBuyingGuide.js
@@ -33,6 +33,8 @@ Sentence Structure: The sentence structure should be varied, with a mix of short
 
 Additionally, use transition words to connect ideas and maintain coherence throughout the article. Always focus on clarity and readability, making it easy for the audience to understand the information presented. Don't follow AI pattern while writing. Make sure to add a bit of humor and add some funny lines.
 
+The goal of this article is: "The goal of an information-based article is to provide valuable, accurate, and relevant information in a clear and engaging manner. Inform and educate the audience, ensuring accuracy and credibility. Engage readers with a well-structured format and valuable insights. Tailor the content to the target audience and consider adding a relevant call-to-action. Keep it concise, SEO-friendly, and prioritize readability and value."
+
 Now write about the "
 
 ${partFromOutline}
@@ -40,8 +42,6 @@ ${partFromOutline}
 " part from the outline.
 
 You should use markdown formatting, including bold, UL/OL, H2, H3, H4 and necessary headings and write with a degree of creativity, burstiness, and human-like. Use proper headings, subheadings, bullet points, etc. Start with H2 Heading. Don't use nested list items. Make bold each heading. Use active voice and short sentences to make the content more readable. I want you to try to include the main keyword "${keyword}" and it's related keyword while writing, do not mention them seperately. Don't Talk about Yourself or Your Experience Address to the Reader.
-
-The goal of this article is: "The goal of an information-based article is to provide valuable, accurate, and relevant information in a clear and engaging manner. Inform and educate the audience, ensuring accuracy and credibility. Engage readers with a well-structured format and valuable insights. Tailor the content to the target audience and consider adding a relevant call-to-action. Keep it concise, SEO-friendly, and prioritize readability and value."
 
 By considering each instruction writing language should be simple and very easy to understand.`;
 
